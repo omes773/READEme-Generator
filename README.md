4a8d5f97317186f041c23d33d6a5125e67f2f036
omes773 <olockhart88@gmail.com>
2023-10-13 23:01:33 -0500

    added questions

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# READEme-Generator
-Homework #7
+## Project Title
+  
+test
+
+![GitHub license](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+
+## Description
+
+test
+
+
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+
+* [License](#license)
+
+
+* [Questions](#questions)
+
+## Installation 
+
+To install necessary dependencies, run the following command:
+
+```
+test
+```
+
+## Usage 
+test
+
+
+## License
+
+This project is licensed under the Apache 2.0 license.
+
+
+## Contributing 
+test
+
+## Tests 
+To run tests, run the following command:
+
+```
+test
+```
+
+
+## Questions 
+
+If you have any questions about the repo, open an issue or contact me directly at test. You can find more of my work at [test](https://github.com/test/).
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generatorMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your Project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a short description of your project",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project? Provide a step-by-step description.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for usage. Include screenshots as needed.",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Explain the guidelines for contributing to this project?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Explain the steps needed to run the tests for your project?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license would you like to use for this project?",
+        choices: ["MIT License", "Apache License 2.0", "GNU General Puplic License v3.0", "Mozilla Public License 2.0", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Profile Link:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address:",
+    },
+];
 
-// Function call to initialize app
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The (Generated)README.md file has beeen created successfully!");
+    });
+}
+
+
+async function init() {
+    console.log("Starting the (Generated)README.md generator...");
+
+
+    const answers = await inquirer.prompt(questions);
+
+
+    let readmeContent = generatorMarkdown(answers);
+
+        
+        writeToFile("(Generated)README.md", readmeContent);
+}
+
+
 init();
